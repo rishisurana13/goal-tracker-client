46fd7d742b9ee90b48217fbbbb2052a05815c7ab
rishisurana13 <rishisurana@hotmail.com>
2019-04-16 21:50:37 -0400

    fix error prod url

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://damp-fjord-80252.herokuapp.com/',
+  production: 'https://damp-fjord-80252.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
